b1bef4d3f4f01cb7c97444862ce37fa2f9be9f82
frumsy <liambritt3@gmail.com>
2018-06-21 13:07:57 -0400

    updated readme and installer

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Awesome
 My Awesome setup
+
+To install simply run:
+git clone https://github.com/frumsy/awesome
+cd awesome
+chmod +x install.sh
+./install.sh
--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ git clone https://github.com/horst3180/arc-icon-theme --depth 1 && cd arc-icon-t
 mv /arc/ /usr/share/icons
 cd ~/.config/awesome
 git clone https://github.com/streetturtle/awesome-wm-widgets.git
+pwd
 echo "The Palace has been built."
