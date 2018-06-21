f110549e1668eb48e95ab3b3098146bc1988e19d
frumsy <liambritt3@gmail.com>
2018-06-21 12:56:40 -0400

    added installer

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ~/.config
+mv ../awesome/ ~/.config/
+cd ~/.config/awesome
+git clone https://github.com/horst3180/arc-icon-theme --depth 1 && cd arc-icon-theme
+mv /arc/ /usr/share/icons
+cd ~/.config/awesome
+git clone https://github.com/streetturtle/awesome-wm-widgets.git
+echo "The Palace has been built."
